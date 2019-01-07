baeb5e8b42c33b0764b2119392ddc9668128ab41
brianwachira <brianwachira7@gmail.com>
2019-01-07 17:38:23 +0300

    set health & computed for future purposes

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,14 @@
 new Vue ({
  el : "#app",
  data : {
-   content : "Injustice"
+   player1 : "You, the guy that knew the monster kill Alfred but still let 'it' live",
+   player2 : "The Monster That Killed almost  half of Justice League",
+   health : 100
+ },
+ computed : {
+   healthbar : function(){
+     return{
+   };
+  }
  }
 });
