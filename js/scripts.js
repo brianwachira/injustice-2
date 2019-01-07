147d94c6c05801066ddfb6f030f94547bd6a6869
brianwachira <brianwachira7@gmail.com>
2019-01-07 18:23:16 +0300

    create objects for different players

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,15 @@
 new Vue ({
  el : "#app",
  data : {
-   player1 : "You, the guy that knew the monster kill Alfred but still let 'it' live",
-   player2 : "The Monster That Killed almost  half of Justice League",
+   show : true,
+   player1 : {
+   name: "You, the guy that knew the monster killed Alfred but still let 'it' live",
    health : 100
+   },
+   player2 : {
+   name : "The Monster That Killed almost  half of Justice League",
+   health : 100
+   }
  },
  computed : {
    healthbar : function(){
