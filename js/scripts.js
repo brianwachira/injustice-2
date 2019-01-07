795d61658d0df151a14ffd9b2b59351c5e6cbf45
brianwachira <brianwachira7@gmail.com>
2019-01-08 00:47:53 +0300

    modify specialAttack damage and add giveIn method

--- js/scripts.js
+++ js/scripts.js
@@ -25,10 +25,10 @@ new Vue({
       }, 2000);
     },
     specialAttack: function() {
-      this.player2.health -= this.calculateDamage(this.player2.health/(Math.max(Math.floor(Math.random() * 5) + 1,1)),5);
+      this.player2.health -= this.calculateDamage(3,20);
       var instance = this;
       setTimeout(function() {
-        instance.player1.health -= instance.calculateDamage(instance.player1.health/(Math.max(Math.floor(Math.random() * 5) + 1, 1)),5) ;
+        instance.player1.health -= instance.calculateDamage(2,20) ;
       }, 2000); 
     },
     heal: function() {  
@@ -39,6 +39,9 @@ new Vue({
           this.player1.health = 100;
           this.player2.health = 100;
         }
+    },
+    giveIn: function(){
+      this.gameHaStarted = false;
     },
     calculateDamage : function(min,max){
       return Math.max(Math.floor(Math.random() * max) + 1, min);
